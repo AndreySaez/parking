92418b64b8cf8adde123b05ba90ade7d8bdf387b
AndreySaez <theleji.andrey@gmail.com>
2024-04-19 11:41:24 +0400

    все команды работаеют вместе

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,20 @@
-@file:Suppress("UNREACHABLE_CODE")
-
 import domain.Manager
 
 fun main(args: Array<String>) {
     println("Введите команду. Список команд: /help")
+    val manager = Manager()
 
-    //domain.Manager().park()
-    // domain.Manager().returnCar()
-    //domain.Manager().parkInfoByCar()
-    Manager().parkInfoByPlace()
+    while (true) {
+        val input = readln()
+        when {
+            input.startsWith("/park") -> manager.park(input)
+            input.startsWith("/return") -> manager.returnCar(input)
+            input.startsWith("/show_car") -> manager.parkInfoByCar(input)
+            input.startsWith("/show_place") -> manager.parkInfoByPlace(input)
+            input.startsWith("/help") -> manager.help(input)
+            else -> println("Unknown command")
+        }
+    }
 }
 
 
